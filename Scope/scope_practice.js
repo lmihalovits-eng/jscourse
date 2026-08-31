aa521343b01ee88805f4f31d055bf350dcd7f657
Levente <lmihalovits@chemaxon.com>
2026-08-31 08:27:16 -0400

    practice

---
+++ Scope/scope_practice.js
@@ -0,0 +1,12 @@
+{
+    let Blocklet = "This is the Blocklet";
+    var Blockvar = "This is the Blockvar";
+    const Blockconst = "This is the Blockconst";
+    Blocklet = "New Blocklet";
+    Blockvar = "New Blockvar";
+    Blockconst = "New Blockconst";
+}
+
+Blocklet = "New Blocklet";
+Blockvar = "New Blockvar";
+Blockconst = "New Blockconst";
