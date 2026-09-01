ad1f7e97fcf102844c76c0fc43de71cd14506e75
Levente <lmihalovits@chemaxon.com>
2026-09-01 08:51:29 -0400

    practice

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,11 @@ function calculateArea() {
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function calculateAmount() {
+ g1 = parseFloat(document.getElementById('grocery1').value);
+ g2 = parseFloat(document.getElementById('grocery2').value);
+ g3 = parseFloat(document.getElementById('grocery3').value);
+
+ let amountresult = g1 + g2 + g3;
+ document.getElementById('amountresult').innerText = `The total amount is:$ ${amountresult}`;
+}
