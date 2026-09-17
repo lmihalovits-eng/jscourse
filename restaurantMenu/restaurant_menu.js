78c58d2647086ee70ed94f492e9c4d1d480dc015
Levente <lmihalovits@chemaxon.com>
2026-09-17 08:32:53 -0400

    ArrayMethodPractice

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $39.99', 'Pasta - $20', 'Burger - $24.99', 'Salmon - $32.99'];
+const dessertMenu = ['Cake - $10', 'Ice Cream - $5', 'Pudding - $6.99', 'Fruit Salad - $9.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
