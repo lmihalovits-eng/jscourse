6ed9a75c213e9622a96cec01b3ea226d18152060
Levente <lmihalovits@chemaxon.com>
2026-09-21 08:49:08 -0400

    StringManipulationPractice

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -38,9 +38,12 @@ startTime = new Date().getTime();
                 wpm = Math.round((typedWords / timeElapsed) * 60);
             }
 
+            var totlength = userTypedText.length;
+
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Length: " + totlength + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
